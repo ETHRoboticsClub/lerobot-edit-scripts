b36b91a2e65e1247c2d07a11124853caff4483f8
baraql <baraqlip@gmail.com>
2026-03-18 20:39:47 +0100

    lot of stuff, still need to add viz

--- scripts/viz.sh
+++ scripts/viz.sh
@@ -1,4 +1,8 @@
-uv run python scripts/viz.py \
-    --repo-id ETHRC/towelspring26 \
-    --episode-index 1 \
-    "$@"
+
+SIZE="${1:-135}"
+
+for episode in $(seq 0 10 "$SIZE"); do
+    uv run lerobot-dataset-viz --mode local \
+        --repo-id ETHRC/towelspring26-cleaned \
+        --episode-index "$episode"
+done
